279ef228a69496d44a5057cb3442d50bef822da8
Graham Campbell <graham@alt-three.com>
2016-09-10 10:05:51 +0100

    Added soap definition file

---
+++ data/soap.php
@@ -0,0 +1,16 @@
+<?php
+return array(
+    'functions' => array(
+        'is_soap_fault',
+        'use_soap_error_handler',
+    ),
+    'classes' => array(
+        'SoapClient',
+        'SoapServer',
+        'SoapFault',
+        'SoapHeader',
+        'SoapParam',
+        'SoapVar',
+    ),
+    'description' => 'The SOAP Extension',
+);
